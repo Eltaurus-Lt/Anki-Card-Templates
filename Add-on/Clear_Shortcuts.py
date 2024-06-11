2f36e4046e28f95d93d761b518fdfe1f421e4e32
Eltaurus-LT02 <Eltaurus@mail.ru>
2024-06-11 12:30:00 +0600

    fix: mch keyboard shortcuts

--- Add-on/Clear_Shortcuts.py
+++ Add-on/Clear_Shortcuts.py
@@ -1,9 +1,23 @@
 from anki.hooks import wrap
 from aqt.reviewer import Reviewer
+#from aqt.qt import Qt # slows down deck loading
 
 def removeShortcuts(self, _old):
-    toRemove = {self.onEnterKey}   
-    shortcuts = [key for key in _old(self) if key[1] not in toRemove]
+    keysToRemove = {
+        "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
+        # Qt.Key.Key_Enter, Qt.Key.Key_Return, 
+        # Qt.Key.Key_Space # does not work?
+        }
+    actionsToRemove = {
+        self.onEnterKey, # Enter
+        #self.on_pause_audio, # 5
+        #self.on_seek_backward, # 6
+        #self.on_seek_forward # 7
+        }
+
+    shortcuts = _old(self)
+    shortcuts = [key for key in shortcuts if key[0] not in keysToRemove]
+    shortcuts = [key for key in shortcuts if key[1] not in actionsToRemove]
 
     return shortcuts
 
