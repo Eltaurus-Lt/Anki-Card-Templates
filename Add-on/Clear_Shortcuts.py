bfc28b598441d6a3ef6e6518fb3d63543b17ca4a
Eltaurus <eltaurus@inbox.lt>
2024-10-12 02:00:30 +0600

    license and copyright notices for add-on

--- Add-on/Clear_Shortcuts.py
+++ Add-on/Clear_Shortcuts.py
@@ -1,3 +1,25 @@
+# This script is part of the [Lt] Card templates Add-on for Anki.
+# Source: github.com/Eltaurus-Lt/Anki-Card-Templates
+# 
+# Copyright (C) 2023-2024 Eltaurus
+# Contact: 
+#     Email: Eltaurus@inbox.lt
+#     GitHub: github.com/Eltaurus-Lt
+#     Anki Forums: forums.ankiweb.net/u/Eltaurus
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 from anki.hooks import wrap
 from aqt import mw
 from aqt.reviewer import Reviewer
