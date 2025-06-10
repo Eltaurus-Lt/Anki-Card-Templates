8c4739066f113cb8d77eccc05e4b19aeeca3f6a0
Eltaurus <eltaurus@inbox.lt>
2025-06-10 08:10:21 +0600

    Save presetes function | extract Memrise Cards creator from Tools Menu and Dialogs

--- Add-on/Tools_Menu.py
+++ Add-on/Tools_Menu.py
@@ -1,27 +1,9 @@
 import aqt
-from . import Dialogs
+from . import Memrise_Cards
 
-from aqt.utils import tooltip
-
-import json
-    
-def NewNoteType():
-
-    dialog = Dialogs.NoteTypeDesigner()
-    if not dialog.exec():
-        return
-    noteType_data = dialog.get_selected_options()
-
-#    json_str = json.dumps(noteType_data)
-#	todo: save to file
-
-    tooltip('creation of a new note type initiated')
-    return
-
-action = aqt.qt.QAction("New Memrise (Lτ) Note Type", aqt.mw)
-action.triggered.connect(NewNoteType)
-
-aqt.mw.form.menuTools.addAction(action)
+newMemriseNT = aqt.qt.QAction("New Memrise (Lτ) Note Type", aqt.mw)
+newMemriseNT.triggered.connect(Memrise_Cards.create)
+aqt.mw.form.menuTools.addAction(newMemriseNT)
 
 # tools_list_menu = aqt.qt.QMenu('Memrise (Lτ) Cards', aqt.mw)
 # tools_list_menu.addAction(action)
---
+++ Add-on/user_files.py
@@ -0,0 +1,38 @@
+# This script is part of the Lt-Cards Add-on for Anki.
+# Source: github.com/Eltaurus-Lt/Anki-Card-Templates
+# 
+# Copyright © 2025 Eltaurus
+# Contact: 
+#     Email: Eltaurus@inbox.lt
+#     GitHub: github.com/Eltaurus-Lt
+#     Anki Forums: forums.ankiweb.net/u/Eltaurus
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
+import os
+
+addon_path = os.path.dirname(__file__)
+
+import os
+
+def save(path, data):
+    file_path = os.path.join(addon_path, "user_files", os.path.normpath(path))
+
+    dir_path = os.path.dirname(file_path)
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+    with open(file_path, "w", encoding="utf-8") as file:
+        file.write(data)
