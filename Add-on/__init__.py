520218ee29b7e5ad9ee1b472be1312a1da648946
Eltaurus <eltaurus@inbox.lt>
2026-02-08 11:53:25 +0600

    generalize injector

--- Add-on/__init__.py
+++ Add-on/__init__.py
@@ -4,7 +4,6 @@ from . import Editor_styles
 from . import Browser_Context_menu
 from . import Tools_Menu
 
-
 from aqt import gui_hooks, mw
 from aqt.deckbrowser import DeckBrowser
 from aqt.overview import Overview
@@ -15,12 +14,24 @@ mw.addonManager.setWebExports(__name__, r"css/.*\.css|js/.*\.js|user_files/(.*\.
 addons_folder = mw.addonManager.addonsFolder()
 addon_name = mw.addonManager.addonFromModule(__name__)
 
+def contextual_injector(web_content: WebContent, context: None):
+    def inject(inj_filename):
+        inj_type = os.path.splitext(inj_filename)[1].lstrip(".").lower()
 
-def inject_to_deck_browser(web_content: WebContent, context: None):
+        if inj_type == "css":
+            target = web_content.css
+        elif inj_type == "js":
+            target = web_content.js
+        else:
+            return
+
+        if os.path.exists(os.path.join(addons_folder, addon_name, inj_type, inj_filename)):
+            target.append(f"/_addons/{addon_name}/{inj_type}/{inj_filename}")
+        if os.path.exists(os.path.join(addons_folder, addon_name, "user_files", inj_filename)):
+            target.append(f"/_addons/{addon_name}/user_files/{inj_filename}")
+
     if isinstance(context, (DeckBrowser, Overview)):
-        web_content.css.append(f"/_addons/{addon_name}/css/deck_styles.css")
-        if os.path.exists(os.path.join(addons_folder, addon_name, "user_files", "deck_styles.css")):
-            web_content.css.append(f"/_addons/{addon_name}/user_files/deck_styles.css")
+        inject("deck_styles.css")
 
 
-gui_hooks.webview_will_set_content.append(inject_to_deck_browser)
+gui_hooks.webview_will_set_content.append(contextual_injector)
