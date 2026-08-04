975097d6a5cd26f104f1b904b2c9e8f2a94b4bd5
Eltaurus <eltaurus@inbox.lt>
2026-08-04 11:32:06 +0600

    import data opt group

--- Add-on/Memrise_Import.py
+++ Add-on/Memrise_Import.py
@@ -119,6 +119,10 @@ class MemriseImportSettings(QDialog):
 
         self.scheduling_checkbox = QCheckBox()
         self.scheduling_checkbox.setChecked(True)
+        self.media_checkbox = QCheckBox()
+        self.media_checkbox.setChecked(True)
+        self.meta_checkbox = QCheckBox()
+        self.meta_checkbox.setChecked(True)
 
         # multiple choice filling
         self.choices_drop = QComboBox()
@@ -146,7 +150,26 @@ class MemriseImportSettings(QDialog):
         layout.addWidget(coursesLable)
         layout.addWidget(self.coursesTable)
 
-        tests_group = QGroupBox("Test types")
+
+        importOptions_group = QGroupBox("Imported data")
+        importOptions_layout = QVBoxLayout()
+        importOptions_group.setLayout(importOptions_layout)
+        layout.addWidget(importOptions_group)
+
+        data_row = QHBoxLayout()
+        data_row.addWidget(QLabel("Learning progress")) 
+        data_row.addWidget(self.scheduling_checkbox)
+        data_row.addSpacing(2 * lh)
+        data_row.addWidget(QLabel("Media"))
+        data_row.addWidget(self.media_checkbox)
+        data_row.addSpacing(2 * lh)
+        data_row.addWidget(QLabel("Metadata")) # description, thumbnails
+        data_row.addWidget(self.meta_checkbox)
+        data_row.addStretch()
+        importOptions_layout.addLayout(data_row)
+
+
+        tests_group = QGroupBox("Testing")
         # todo tooltip : at least 1 typing-tapping should be enabled, applicable to newly created note types only
         tests_layout = QVBoxLayout()
         tests_group.setLayout(tests_layout)
@@ -170,47 +193,37 @@ class MemriseImportSettings(QDialog):
         tests_row2.addStretch()
         tests_layout.addLayout(tests_row2)
 
-
-        scheduling_row = QHBoxLayout()
-        scheduling_row.addWidget(QLabel("Import Learning progress"))
-        scheduling_row.addWidget(self.scheduling_checkbox)
-        scheduling_row.addStretch()
-        layout.addLayout(scheduling_row)
-
         # ▼ Advanced
 
-        choices_row = QHBoxLayout()
-        choices_row.addWidget(QLabel("Choices: "))
-        choices_row.addWidget(self.choices_drop)
-        choices_row.addStretch()
-        layout.addLayout(choices_row)
+        # choices_row = QHBoxLayout()
+        # choices_row.addWidget(QLabel("Choices: "))
+        # choices_row.addWidget(self.choices_drop)
+        # choices_row.addStretch()
+        # layout.addLayout(choices_row)
 
-        reverse_row = QHBoxLayout()
-        reverse_row.addWidget(QLabel("Reverse Cards: "))
-        reverse_row.addWidget(self.reverse_drop)
-        reverse_row.addStretch()
-        layout.addLayout(reverse_row)
-
-
-        subdecks_group = QGroupBox("Subdecks")
-        subdecks_layout = QVBoxLayout()
-        subdecks_group.setLayout(subdecks_layout)
-        layout.addWidget(subdecks_group)
-
-        subdeck_row = QHBoxLayout()
-        subdeck_row.addWidget(QLabel("Replicate folder tree"))
-        subdeck_row.addWidget(self.subdecks_checkbox)
-        subdeck_row.addStretch()
-        subdecks_layout.addLayout(subdeck_row)
-
-        levels_row = QHBoxLayout()
-        levels_row.addWidget(QLabel("Subdecks from levels"))
-        levels_row.addWidget(self.levels_checkbox)
-        levels_row.addStretch()
-        subdecks_layout.addLayout(levels_row)
+        # reverse_row = QHBoxLayout()
+        # reverse_row.addWidget(QLabel("Reverse Cards: "))
+        # reverse_row.addWidget(self.reverse_drop)
+        # reverse_row.addStretch()
+        # layout.addLayout(reverse_row)
 
 
-#############################
+        # subdecks_group = QGroupBox("Subdecks")
+        # subdecks_layout = QVBoxLayout()
+        # subdecks_group.setLayout(subdecks_layout)
+        # layout.addWidget(subdecks_group)
+
+        # subdeck_row = QHBoxLayout()
+        # subdeck_row.addWidget(QLabel("Replicate folder tree"))
+        # subdeck_row.addWidget(self.subdecks_checkbox)
+        # subdeck_row.addStretch()
+        # subdecks_layout.addLayout(subdeck_row)
+
+        # levels_row = QHBoxLayout()
+        # levels_row.addWidget(QLabel("Subdecks from levels"))
+        # levels_row.addWidget(self.levels_checkbox)
+        # levels_row.addStretch()
+        # subdecks_layout.addLayout(levels_row)
 
         # Ok/Cancel + Help
         button_help = QPushButton("Help")
